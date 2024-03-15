659534356085a9e36df8f609caf8ffbd6d9d28a4
dramazzan <ramazandautbek04@gmail.com>
2024-03-15 21:25:49 +0500

    week 2

--- src/Factory/Main.java
+++ src/Factory/Main.java
@@ -2,12 +2,14 @@ package Factory;
 
 public class Main {
 
-
     public static void main(String[] args) {
          CarFactory carFactory = CarFactoryCreater.createCarByType("cOupe");
+        CarFactory car1Factory = CarFactoryCreater.createCarByType("Truck");
          Car car = carFactory.createCar();
+         Car car1 = car1Factory.createCar();
 
          car.driveCar();
+         car1.driveCar();
     }
 
 
